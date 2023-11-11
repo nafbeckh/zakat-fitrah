e94f186cc54236a35716a427d6d9b599efc22fde
nafbeckh <nafbeckhshn@gmail.com>
2023-11-11 21:11:38 +0700

    Create login method

--- app/User.php
+++ app/User.php
@@ -7,6 +7,39 @@ use Config\Connection;
 class User extends Connection
 {
 
+  public function login()
+  {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = 'SELECT username, nama, password FROM users WHERE username = :username';
+    $stmt = $this->db->prepare($sql);
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
+
+    if (!$stmt->rowCount() > 0) {
+      return [
+        "status" => "fail",
+        "message" => "Username atau Password ada salah"
+      ];
+    }
+
+    $user = $stmt->fetch();
+
+    $verifyPassword = password_verify($password, $user['password']);
+
+    if (!$verifyPassword) {
+      return [
+        "status" => "fail",
+        "message" => "Username atau Password ada salah"
+      ];
+    }
+
+    $_SESSION['auth']['username'] = $user['username'];
+    $_SESSION['auth']['name'] = $user['nama'];
+
+    return ["status" => "success"];
+  }
 }
 
 ?>
