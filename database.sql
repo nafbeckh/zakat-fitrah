c0a6362719735ad451311a49b74864f5f9ce63e3
nafbeckh <nafbeckhshn@gmail.com>
2023-11-12 10:45:49 +0700

    Update sql

--- database.sql
+++ database.sql
@@ -7,6 +7,9 @@ CREATE TABLE users(
   `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+INSERT INTO users (`id`, `username`, `password`, `nama`)
+  VALUES ('syj9AUTpXoQsgU6n', 'admin', '$2y$10$OD.BmC4pHuhxUZ8hoQvo3eZhOWEYtcac8Di3PAjlhhJYTbXWrmqHG', 'Administrator');
+
 CREATE TABLE amils(
   `id` VARCHAR(16) NOT NULL PRIMARY KEY,
   `nama` VARCHAR(100) NOT NULL,
@@ -31,8 +34,8 @@ CREATE TABLE pembayarans(
   `muzakki_id` VARCHAR(16) NOT NULL,
   `jumlah_tanggungan` INT NOT NULL,
   `jenis_bayar` ENUM('Beras', 'Tunai') NOT NULL DEFAULT "Tunai",
-  `beras`FLOAT NOT NULL,
-  `tunai`INT NOT NULL,
+  `beras`FLOAT NOT NULL DEFAULT 0,
+  `tunai`INT NOT NULL DEFAULT 0,
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   
