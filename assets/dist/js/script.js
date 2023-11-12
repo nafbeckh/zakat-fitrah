d06c6df37dd433407b4b409f4964f0916f5a5d23
nafbeckh <nafbeckhshn@gmail.com>
2023-11-12 10:47:40 +0700

    Update style & script

--- assets/dist/js/script.js
+++ assets/dist/js/script.js
@@ -1,8 +1,8 @@
 const deleteRow = (id) => {
-  let form = document.getElementById("formDelete");
-  let input = document.getElementById("id");
+  let form = document.getElementById('formDelete')
+  let input = document.getElementById('id')
 
-  let result = confirm("Apakah anda ingin menghapus data ini?");
+  let result = confirm('Apakah anda ingin menghapus data ini?')
 
   if (result) {
     input.value = id
@@ -11,7 +11,34 @@ const deleteRow = (id) => {
 }
 
 const logout = (id) => {
-  let form = document.getElementById("formLogout");
-  let input = document.getElementById("logout");
+  let form = document.getElementById('formLogout')
+  let input = document.getElementById('logout')
   form.submit()
+}
+
+const jenisBayar = () => {
+  const select = document.getElementById('jenis_bayar')
+  const beras = document.getElementById("Beras")
+  const tunai = document.getElementById("Tunai")
+
+  if (select.value == "") {
+    beras.style = 'display: none'
+    tunai.style = 'display: none'
+  }
+
+  if (select.value == "Beras") {
+    beras.style = 'display: block'
+    beras.setAttribute('require', 'require')
+
+    tunai.style = 'display: none'
+    tunai.removeAttribute('require')
+  }
+
+  if (select.value == "Tunai") {
+    tunai.style = 'display: block'
+    tunai.setAttribute('require', 'require')
+
+    beras.style = 'display: none'
+    beras.removeAttribute('require')
+  }
 }
